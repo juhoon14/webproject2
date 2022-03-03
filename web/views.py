e72ec5baf776e15617348c35788ffbd95bf3a80e
juhoon14 <juhoon14@gmail.com>
2022-03-03 16:03:57 +0900

    Initial commit

---
+++ web/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views import View
+from django_request_mapping import request_mapping
+# Create your views here.
+
+
+
+@request_mapping("")
+class MyView(View):
+
+    @request_mapping("/", method="get")
+    def index(self,request):
+        return render(request,'index.html');
+
