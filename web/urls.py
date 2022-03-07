97e871daa459a12973fbcdbdd584cd302fd5d4c0
Jinyoung-seok <99372008+Jinyoung-seok@users.noreply.github.com>
2022-03-07 17:30:05 +0900

    Created Item detail pages, Changed pictures, Changed some prices

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,11 @@
 from django_request_mapping import UrlPattern
 
 from web.views import MyView
+from web.views_top import TopView
+
+from web.views_bottom import BottomView
 
 urlpatterns = UrlPattern();
 urlpatterns.register(MyView);
+urlpatterns.register(TopView);
+urlpatterns.register(BottomView);
